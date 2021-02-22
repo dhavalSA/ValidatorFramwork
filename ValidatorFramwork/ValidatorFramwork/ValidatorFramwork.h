0d5c3430fb16ef3651a2088e28658e4f24a21572
Dhaval Soni <dhaval.soni@solutionanalysts.com>
2021-02-22 20:57:43 +0530

    framwork added

---
+++ ValidatorFramwork/ValidatorFramwork/ValidatorFramwork.h
@@ -0,0 +1,18 @@
+//
+//  ValidatorFramwork.h
+//  ValidatorFramwork
+//
+//  Created by Dhaval Soni on 22/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ValidatorFramwork.
+FOUNDATION_EXPORT double ValidatorFramworkVersionNumber;
+
+//! Project version string for ValidatorFramwork.
+FOUNDATION_EXPORT const unsigned char ValidatorFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValidatorFramwork/PublicHeader.h>
+
+
